4a1ab91f206fc29ba2d993acfedde647d741f13a
Ubuntu <ubuntu@ip-172-31-30-196.us-east-2.compute.internal>
2021-02-12 17:18:33 +0000

    add devise

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: bundle exec rails server -p 8080
+webpack: bin/webpack-dev-server
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 
+  devise_for :users
   root to: 'posts#index'
   resources :posts
   resources :replies
