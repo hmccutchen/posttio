c2fcb7219ce21d0129fe1e009d421e61c4ec6225
Ubuntu <ubuntu@ip-172-31-30-196.us-east-2.compute.internal>
2021-02-14 15:11:36 +0000

    create image avatar for user

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: bundle exec rails server -p 3000
+web: bundle exec rails server -p 8080
 webpack: bin/webpack-dev-server
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, 
+    
+if: :devise_controller?
+    def configure_permitted_parameters  
+     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:image_avatar, :email, :password, :password_confirmation, :remember_me, :current_password) 
+    } 
+    end
 end
