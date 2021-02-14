39f96138e179c35a15da21480683716e731b0882
Ubuntu <ubuntu@ip-172-31-30-196.us-east-2.compute.internal>
2021-02-14 17:32:31 +0000

    add s3 bucket for active storage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, 
+     before_action :configure_permitted_parameters, if: :devise_controller?
     
-if: :devise_controller?
-    def configure_permitted_parameters  
-     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:image_avatar, :email, :password, :password_confirmation, :remember_me, :current_password) 
-    } 
-    end
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:image_avatar, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
